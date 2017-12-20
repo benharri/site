bf9a669417c91959a372ef729c01a53c934a82aa
Ben Harris <bharris@hagerty.com>
2017-12-20 15:36:28 -0500

    prettified html

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
-   <!-- footer
+<!-- footer
    ================================================== -->
-   <footer>
+<footer>
 
-      <div class="row">
+    <div class="row">
 
-         <div class="twelve columns">
+        <div class="twelve columns">
 
             <?php include 'social.html'; ?>
 
@@ -12,27 +12,31 @@
                 <li>&copy; Copyright <?=date("Y")?> Benjamin Harris</li>
             </ul>
 
-         </div>
+        </div>
 
-         <div id="go-top"><a class="smoothscroll" title="Back to Top" href="#home"><i class="icon-up-open"></i></a></div>
+        <div id="go-top">
+            <a class="smoothscroll" title="Back to Top" href="#home">
+                <i class="icon-up-open"></i>
+            </a>
+        </div>
 
-      </div>
+    </div>
 
-   </footer> <!-- Footer End-->
+</footer>
+<!-- Footer End-->
 
-   <!-- Java Script
+<!-- Java Script
    ================================================== -->
-   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-   <script>window.jQuery || document.write('<script src="js/jquery-1.10.2.min.js"><\/script>')</script>
-   <script type="text/javascript" src="js/jquery-migrate-1.2.1.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/jquery-1.10.2.min.js"><\/script>')</script>
+<script type="text/javascript" src="js/jquery-migrate-1.2.1.min.js"></script>
 
-   <script src="js/jquery.flexslider.js"></script>
-   <script src="js/waypoints.js"></script>
-   <script src="js/jquery.fittext.js"></script>
-   <script src="js/magnific-popup.js"></script>
-   <script src="js/init.js"></script>
+<script src="js/jquery.flexslider.js"></script>
+<script src="js/waypoints.js"></script>
+<script src="js/jquery.fittext.js"></script>
+<script src="js/magnific-popup.js"></script>
+<script src="js/init.js"></script>
 
 </body>
 
 </html>
-
